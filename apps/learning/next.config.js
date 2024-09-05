df6be59fb94f0ab733934afe33ccc179d0da842b
Reuben Armstrong <reuben@reubarm.com>
2024-09-05 09:07:04 +0100

    next config

--- apps/learning/next.config.js
+++ apps/learning/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  images: {
+    domains: ['charity.marketing'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
